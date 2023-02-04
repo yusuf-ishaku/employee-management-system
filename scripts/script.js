53d820321090c0a421ca748ccca0402f6cd8f80f
spolonko <kingshakzy@gmail.com>
2023-02-04 23:50:00 +0100

    Landing page design done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log("viad")
+// Get all the access handles
+let persons = document.querySelectorAll(".person");
+
+// Loop through
+persons.forEach((person) =>{
+    // Add a click function
+    person.addEventListener("click", ()=>{
+        // click function checks the text content of the first child to determine which user was clicked
+       if(person.firstElementChild.textContent === "Person 1"){
+        console.log("admin clicked")
+       }
+       else if(person.firstElementChild.textContent === "Person 2"){
+        console.log("Manager clicked")
+       }
+       else if(person.firstElementChild.textContent === "Person 3"){
+        console.log("HR clicked")
+       }
+    })
+})
