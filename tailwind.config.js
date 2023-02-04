b7cefc020f7f0ad115645140b95faa489cc1404a
spolonko <kingshakzy@gmail.com>
2023-02-04 22:52:28 +0100

    Added necessary tools

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./scripts/script.js",
+    "./index.html",
+    "./css/style.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
